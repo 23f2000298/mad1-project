818139d80b460691a79c300e8ed7910019ccfcec
Joydev <23f2000298@ds.study.iitm.ac.in>
2025-07-24 19:08:17 +0530

    24-07-2025

--- README.md
+++
@@ -1,7 +0,0 @@
-# Parking App
-
-## step1 - Installing the required things
-'pip install -r requirements.txt'
-
-## step 2 - To Run the app
-'python app.py'
---
+++ application/models.py
@@ -0,0 +1,36 @@
+from .database import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id  = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(),unique = True,nullable = False)
+    email = db.Column(db.String(),unique = True,nullable = False)
+    password = db.Column(db.String(),nullable = False)
+    type = db.Column(db.String(),default = "general")
+    parkinglot = db.relationship("ParkingLot",backref = "bearer")
+
+   
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_no = db.Column(db.String(80), nullable=False)
+    spot_id = db.Column(db.Integer, nullable=False)
+    lot_id = db.Column(db.Integer, db.ForeignKey("parking_lot.id"),nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable = False)
+    parking_time = db.Column(db.DateTime,default = datetime.utcnow)
+    release_time = db.Column(db.DateTime,nullable = True)
+    total_cost = db.Column(db.Float,nullable = True)
+
+    available = db.Column(db.Boolean,default = True) #new
+    # parking_lot = db.relationship("ParkingLot",backref = "bookings")
+
+class ParkingLot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    address = db.Column(db.String(80), nullable=False)
+    pincode = db.Column(db.Integer, nullable=False)
+    price_per_hour = db.Column(db.Integer, nullable=False)
+    maximum_spot = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable = False)
+    bookings = db.relationship("Booking",backref = "parking_lot",cascade = "all",passive_deletes = True)
